1daefa4e48b4752d2cd2d42e0019b6b6d0ece19e
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2020-03-20 23:49:34 +0100

    Text changes

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -59,7 +59,7 @@ class HomeView extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
                           Text(
-                            'FLUTTER WEB.\nTHE BASICS',
+                            'FLUTTER WEB.\nTHE BASICS.',
                             style: TextStyle(
                               fontWeight: FontWeight.w800,
                               fontSize: 80,
